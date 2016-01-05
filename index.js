47de6fbd5410599bb4bf9955e5a81be06da0e58b
Igor Khomenko <igor@quickblox.com>
2016-01-05 23:16:13 +0200

    added Slack API integration

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
 // var Parser = require('./parser');
 // var HTTPService = require('./http_service');
 var FeedparserService = require('./feedparser_service');
+var SlackService = require('./slack_service');
 
 
 var tag = "quickblox";
+var additionalTags = ["ios"];
 var stackoverflowFeedUrl = "http://stackoverflow.com/feeds/tag/";
 
 var feedParser = new FeedparserService();
-feedParser.parse(stackoverflowFeedUrl + tag, function(result){
-    console.log(result);
+feedParser.parse(stackoverflowFeedUrl + tag, function(items){
+
+    items.forEach(function(item, i, arr) {
+      console.log("TITLE: " + item.title);
+      console.log("CATEGORIES: " + item.categories);
+      console.log("LINK: " + item.link);
+      console.log("UPDATED: " + item.date);
+      console.log("PUBDATE: " + item.pubdate);
+    });
+
+    var it = items[0];
+    // var data = {"text": text};
+
+    var data = {attachments: [{
+                                pretext: "New activity. <" + it.link + "|Check it>",
+                                fields: [{title: "Title", value: it.title, short: false},
+                                         {title: "Categories", value: it.categories.join(", "), short: false},
+                                         {title: "Updated date", value: it.date, short: true},
+                                         {title: "Pub date", value: it.pubdate, short: true}
+                                       ]
+                              }]
+                            };
+
+    var slackAPI = new SlackService();
+    slackAPI.fire(data,
+      function(){
+        console.log("Ok!");
+      },function(error){
+        console.error(error);
+      });
+
   },function(error){
     console.error(error);
   }
 );
-
-// var httpService = new HTTPService();
-// httpService.get(stackoverflowFeedUrl + tag,
-//   function(result){
-//     console.log("data has downloaded, parsing it..");
-//
-//     var parser = new Parser();
-//     parser.parse(result,
-//       function(result){
-//         console.log(result);
-//       },function(error){
-//         console.error(error);
-//       }
-//     );
-//
-//   },function(error){
-//     console.error(error);
-//   }
-// );
---
+++ slack_service.js
@@ -0,0 +1,27 @@
+var request = require("request");
+
+// SlackService constructor.
+var SlackService = function() {
+  this.webhookUrl = "";
+};
+
+// HTTPService get method
+SlackService.prototype.fire = function(data, successCallback, errorCallback) {
+  var self = this;
+
+  request.post({
+    headers: {'content-type' : 'application/json'},
+    url:     self.webhookUrl,
+    body:    JSON.stringify(data)
+  }, function(error, response, body){
+    if(error){
+      errorCallback(error);
+    }else{
+      successCallback(body);
+    }
+  });
+
+};
+
+// Export the SlackService constructor from this module.
+module.exports = SlackService;
