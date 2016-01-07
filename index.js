629ef203b66ad9a920deb92521595c8203ac5ba9
Igor Khomenko <igor@quickblox.com>
2016-01-07 17:30:35 +0200

    finished

--- index.js
+++ index.js
@@ -4,41 +4,62 @@ var FeedparserService = require('./feedparser_service');
 var SlackService = require('./slack_service');
 var CONFIG = require('./config');
 
-var tag = "quickblox";
-var additionalTags = ["ios"];
 var stackoverflowFeedUrl = "http://stackoverflow.com/feeds/tag/";
 
-var feedParser = new FeedparserService();
-feedParser.parse(stackoverflowFeedUrl + tag, function(items){
+start();
 
-    items.forEach(function(item, i, arr) {
-      console.log("TITLE: " + item.title);
-      console.log("CATEGORIES: " + item.categories);
-      console.log("LINK: " + item.link);
-      console.log("UPDATED: " + item.date);
-      console.log("PUBDATE: " + item.pubdate);
-    });
+function start(){
+  var feedParser = new FeedparserService();
+  feedParser.parse(stackoverflowFeedUrl + CONFIG.mainTag, function(entries){
 
-    var it = items[0];
-    var data = {attachments: [{
-                                pretext: "New activity. <" + it.link + "|Check it>",
-                                fields: [{title: "Title", value: it.title, short: false},
-                                         {title: "Categories", value: it.categories.join(", "), short: false},
-                                         {title: "Updated date", value: it.date, short: true},
-                                         {title: "Pub date", value: it.pubdate, short: true}
-                                       ]
-                              }]
-                            };
+      var slackAPI;
 
-    var slackAPI = new SlackService(CONFIG.webhookUrl);
-    slackAPI.fire(data,
-      function(){
-        console.log("Success!");
-      },function(error){
-        console.error(error);
+      entries.forEach(function(entry, i, arr) {
+        if(isNewEntry(entry) && isEntryHasNeededTags(entry)){
+          console.log("New Entry found! Date: " + entry.date + ". Title: " + entry.title);
+
+          if(!slackAPI){
+            slackAPI = new SlackService(CONFIG.webhookUrl);
+          }
+
+          var message = slackAPI.buildMessage(entry)
+
+          slackAPI.fire(message,
+            function(){
+              console.log("Message has pushed successfully.");
+            },function(error){
+              console.error(error);
+            });
+        }
       });
 
-  },function(error){
-    console.error(error);
+    },function(error){
+      console.error(error);
+    }
+  );
+}
+
+function isNewEntry(entry){
+  var entryTimestamp = entry.date.getTime();
+  var currentTimestamp = Date.now();
+
+  return currentTimestamp-entryTimestamp <= CONFIG.runIntervalInMilliseconds;
+}
+
+function isEntryHasNeededTags(entry){
+
+  for(var i = 0; i < entry.categories.length; i++){
+    var entryTag = entry.categories[i];
+
+    for(var j = 0; j < CONFIG.additionalTags.length; j++){
+      var tagToCheck = CONFIG.additionalTags[j];
+
+      if(entryTag.indexOf(tagToCheck) > -1){
+        return true;
+      }
+    }
+
   }
-);
+
+  return false;
+}
--- slack_service.js
+++ slack_service.js
@@ -5,7 +5,21 @@ var SlackService = function(slackWebhookUrl) {
   this.webhookUrl = slackWebhookUrl;
 };
 
-// HTTPService get method
+SlackService.prototype.buildMessage = function(feedEntry){
+  var message = {attachments: [{
+                              pretext: "New activity. <" + feedEntry.link + "|Check it>",
+                              fallback: "New activity",
+                              fields: [{title: "Title", value: feedEntry.title, short: false},
+                                       {title: "Categories", value: feedEntry.categories.join(", "), short: false},
+                                       {title: "Updated date", value: new Date(feedEntry.date).toString(), short: true},
+                                       {title: "Publish date", value: new Date(feedEntry.pubdate).toString(), short: true}
+                                     ]
+                            }]
+                          };
+
+  return message;
+}
+
 SlackService.prototype.fire = function(data, successCallback, errorCallback) {
   var self = this;
 
